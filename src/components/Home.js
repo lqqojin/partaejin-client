0bd6f4e45ab1f92e6ffd25124b5f13a65f50d8c8
lqqojin <lqqojin@gmail.com>
2020-12-05 17:08:32 +0900

    폴터 구조 v0.1

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: '100vh',
+        }}>
+            <h1>[ 공사] 중입니다</h1>
+        </div>
+    )
+}
+
+export default Home;
